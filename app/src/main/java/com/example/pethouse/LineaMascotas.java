c2b3a5487f00f022309171e8469619c40a08afde
alfredzero <amezcuaalfredo81@gmail.com>
2024-05-07 21:22:49 -0700

    raised button

--- app/src/main/java/com/example/pethouse/LineaMascotas.java
+++ app/src/main/java/com/example/pethouse/LineaMascotas.java
@@ -1,7 +1,6 @@
 package com.example.pethouse;
 
 import android.os.Bundle;
-import android.view.Menu;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LineaMascotas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
+        setContentView(R.layout.liinea_mascotas);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true ;
     }
 }
