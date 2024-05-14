79625e79b1184c346582dd71210f8088fa8210e8
alfredzero <amezcuaalfredo81@gmail.com>
2024-05-13 20:44:09 -0700

    add float btn

--- app/src/main/java/com/example/pethouse/MainActivity.java
+++ app/src/main/java/com/example/pethouse/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.pethouse;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +10,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
+        addFloatBTN();
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
@@ -29,5 +33,15 @@ public class MainActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.menu, menu);
         return true ;
+    }
+
+    public void addFloatBTN(){
+        FloatingActionButton myFloat = (FloatingActionButton) findViewById(R.id.floating_button);
+        myFloat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
